b3a6f4811f11808ebdc514bd868c4bc3bff09f55
realview45 <kjink0125@gmail.com>
2026-01-29 16:51:23 +0900

    first

--- src/main/java/com/beyond/order/BasicApplication.java
+++ src/main/java/com/beyond/order/BasicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderApplication {
+public class BasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderApplication.class, args);
+		SpringApplication.run(BasicApplication.class, args);
 	}
 
 }
