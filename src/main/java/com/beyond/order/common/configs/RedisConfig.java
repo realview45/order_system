32427462de51a3d3b9ffcc9899ab6745dc0d4b32
realview45 <kjink0125@gmail.com>
2026-02-03 11:31:13 +0900

    redis pubsub

--- src/main/java/com/beyond/order/common/configs/RedisConfig.java
+++ src/main/java/com/beyond/order/common/configs/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.beyond.order.common.configs;
 
+import com.beyond.order.common.service.SseAlarmService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -59,5 +63,44 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());//다문자열이지만 이건리스트 셋,해시스,지셋이라는 태그를 가진다.
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
+    }
+
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisConnectionFactory ssePubSubConnectionFactory(){
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        //redis pub/sub기능은 db에 값을 저장하는 기능이 아니므로, 특정db에 의존적이지 않음.
+        return new LettuceConnectionFactory();
+    }
+    @Bean//이 빈을 주입받아 redis에 저장할 예정
+    @Qualifier("ssePubSub")
+    public RedisTemplate<String, String>ssePubSubRedisTemplate(@Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());//다문자열이지만 이건리스트 셋,해시스,지셋이라는 태그를 가진다.
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+//    redis 리스너(subscribe) 객체
+//                   listen중
+//    호출구조 RedisMessageListenerContainer -> MessageListenerAdapter -> SseAlarmService(MessageListener객체를 구현한)
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisMessageListenerContainer redisMessageListenerContainer(@Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory,
+                                                                       @Qualifier("ssePubSub") MessageListenerAdapter messageListenerAdapter){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(messageListenerAdapter, new PatternTopic("order-channel"));
+//        만약에 여러채널을 구독해야 하는 경우, 여러개의 PatternTopic을 add하거나, 별도의 Listener Bean 객체 생성
+        return container;
+    }
+//    redis에서 수신된 메시지를 처리하는 객체
+    @Bean
+    @Qualifier("ssePubSub")
+    public MessageListenerAdapter messageListenerAdapter(SseAlarmService sseAlarmService){
+//        채널로부터 수신되는 message처리를 SseAlarmService의 onMessage메서드로 위임
+        return new MessageListenerAdapter(sseAlarmService, "onMessage");
     }
 }
--- src/main/java/com/beyond/order/common/service/SseAlarmService.java
+++ src/main/java/com/beyond/order/common/service/SseAlarmService.java
@@ -4,30 +4,52 @@ import com.beyond.order.common.dtos.SseMessageDto;
 import com.beyond.order.common.repository.SseEmitterRegistry;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.batch.item.mail.MailErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 
 @Component
-public class SseAlarmService {
+public class SseAlarmService implements MessageListener {
     private final SseEmitterRegistry sseEmitterRegistry;
     private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, String> redisTemplate;
     @Autowired
-    public SseAlarmService(SseEmitterRegistry sseEmitterRegistry, ObjectMapper objectMapper) {
+    public SseAlarmService(SseEmitterRegistry sseEmitterRegistry, ObjectMapper objectMapper, @Qualifier("ssePubSub")RedisTemplate<String, String> redisTemplate) {
         this.sseEmitterRegistry = sseEmitterRegistry;
         this.objectMapper = objectMapper;
+        this.redisTemplate = redisTemplate;
     }
 
     public void sendMessage(String receiver, String sender, String message) {
         SseEmitter sseEmitter = sseEmitterRegistry.getEmitter(receiver);
+        //없을때 아래 dto를 redis로 전송해야함
         SseMessageDto dto = SseMessageDto.builder()
                 .receiver(receiver).sender(sender).message(message).build();
         try {
             String data = objectMapper.writeValueAsString(dto);
                                         //메시지의 타이틀     본문
-            sseEmitter.send(SseEmitter.event().name("ordered").data(data));
+//            sseEmitter.send(SseEmitter.event().name("ordered").data(data));
+//            redis pubsub기능을 활용하여 메시지 publish
+            redisTemplate.convertAndSend("order-channel", data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String channelName = new String(pattern);
+        System.out.println("channelName : " + channelName);
+        try {
+            SseMessageDto dto = objectMapper.readValue(message.getBody(), SseMessageDto.class);
+            System.out.println("message : " + dto);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
